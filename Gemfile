b015307faef1aef069e9edf15d65b23be95004ad
Nathan Carnes <nathan@carnesmedia.com>
2012-08-31 13:19:18 -0700

    yay data persistence

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source "https://rubygems.org"
 
 gem "cinch"
 gem "heroku"
+
+gem "data_mapper"
+gem "dm-sqlite-adapter"
+gem "dm-postgres-adapter"
--- beerbot.rb
+++ beerbot.rb
@@ -1,16 +1,32 @@
 require 'cinch'
+require 'data_mapper'
+require 'dm-sqlite-adapter'
+require 'dm-postgres-adapter'
 
 class BeerBot
-  def self.add_beer sender, recipient
+  include DataMapper::Resource
+  property :id, Integer, serial: true
+  property :sender, String
+  property :recipient, String
+  property :count, Integer, default: 0
 
+  def self.add_beer sender, recipient
+    round = self.first_or_create(sender: sender, recipient: recipient)
+    round.update(count: round.count + 1)
+
+    round.count
   end
 
   def self.cash_in sender, recipient
+    round = self.first_or_create(sender: sender, recipient: recipient)
+    return false unless round.count > 0
+    round.update(count: round.count - 1)
 
+    round.count
   end
 
   def self.status nick
-    'No one.'
+    return self.all(:recipient => nick, :count.gt => 0)
   end
 end
 
@@ -24,22 +40,38 @@ bot = Cinch::Bot.new do
   # Add a drink
   on :message, /.*i owe (\S*) a (beer|drink).*/i do |m|
     recipient = m.message.match(/.*i owe (.*) a (beer|drink|beverage).*/i)[1]
-    m.reply "Duly noted, #{m.user.nick}. You owe a delicious beverage to #{recipient}."
 
-    BeerBot.add_beer m.user.nick, recipient
+    round = BeerBot.add_beer m.user.nick, recipient
+
+    if round > 1
+      m.reply "Got it, #{m.user.nick}. You owe a #{round} drinks to #{recipient}. Better get buying."
+    else
+      m.reply "Duly noted, #{m.user.nick}. You owe a delicious beverage to #{recipient}."
+    end
   end
 
   # Take one down
   on :message, /(\S*) (paid up|(bought.*(beer|drink|beverage))).*/i do |m|
     sender = m.message.match(/(\S*) (paid up|(bought.*(beer|drink|beverage))).*/i)[1]
 
-    m.reply "Glad to see #{sender} isn't as much of a deadbeat as I thought."
-    BeerBot.cash_in sender, recipient
+    round = BeerBot.cash_in sender, m.user.nick
+
+    if round
+      m.reply "Glad to see #{sender} isn't as much of a deadbeat as I thought."
+    else
+      m.reply "Well this is awkward. #{sender} didn't owe you anything. Poor lifestyle choices all around."
+    end
   end
 
   # Pass it around
-  on :message, /.*who owes me (beer|beverages|drink).*/i do |m|
-    m.reply BeerBot.status m.user.nick
+  on :message, /.*who owes me.*(beer|beverages|drink).*/i do |m|
+    status = BeerBot.status m.user.nick
+
+    if status
+      m.reply status.map{ |round| "#{round.sender} (#{round.count})"}.join(', ')
+    else
+      m.reply 'No one owes you beer. You should really be nicer to people.'
+    end
   end
 
   # Help
@@ -49,5 +81,12 @@ bot = Cinch::Bot.new do
     m.reply 'You can see who owes you drinks by saying "Who owes me drinks?"'
   end
 end
+
+
+# Initialization
+DataMapper.finalize
+
+DataMapper.setup(:default, (ENV["DATABASE_URL"] || "sqlite3:///#{Dir.pwd}/development.sqlite3"))
+DataMapper.auto_upgrade!
 
 bot.start
